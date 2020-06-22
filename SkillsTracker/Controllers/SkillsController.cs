7c0376524fe03c7e4fba237c7fabf03659027f03
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-22 13:20:09 -0400

    Job done

--- SkillsTracker/Controllers/SkillsController.cs
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -24,18 +24,18 @@ namespace SkillsTracker.Controllers
         }
 
         [HttpGet("form")]
-        [HttpPost]
-        public IActionResult Search()
+        
+        public IActionResult ProgressTracker()
         {
-            string progressTracker = "<option value='1'>Learning basics</option>" +
-                "<option value='2'>Making apps</option>" +
-                "<option value='3'>Master coder</option>";
+            string progressTracker = "<option value='Learning basics'>Learning basics</option>" +
+                "<option value='Making apps'>Making apps</option>" +
+                "<option value='Master coder'>Master coder</option>";
 
             string html = "<form method='post' action='/skills/form'>" +
                 "<h2>Date:</h2>" +
                 "<input type='date' name='date'/>" +
                 "<h2>C#:</h2>" +
-                "<select name='csharp'>" +
+                "<select name='cSharp'>" +
                     progressTracker +
                 "</select>" +
                 "<h2>Python:</h2>" +
@@ -45,8 +45,21 @@ namespace SkillsTracker.Controllers
                 "<h2>Ruby:</h2>" +
                 "<select name='ruby'>" +
                     progressTracker +
-                "</select>" +
+                "</select>" + "<br><br>" +
+                "<input type='submit' value='Log Progress'/>" +
                 "</form>";
+            return Content(html, "text/html");
+        }
+
+        [HttpPost("form")]
+        public IActionResult TrackProgress(string date, string cSharp, string python, string ruby)
+        {
+            string html = $"<h1>{date}</h1>" +
+                "<ol>" +
+                    $"<li>C#: {cSharp}</li>" +
+                    $"<li>Python: {python}</li>" +
+                    $"<li>Ruby: {ruby}</li>" +
+                "</ol>";
             return Content(html, "text/html");
         }
     }
